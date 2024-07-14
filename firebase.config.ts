f6f53dec68847717b66fdf7ab558a5d34cf546ae
gtanchak <ghtanchak77@gmail.com>
2024-07-14 14:59:00 +0530

    Feat: configure the firebase

---
+++ firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3RguL_srE1jPbdAEPQ_Ael1fwVMkyLDI",
+  authDomain: "movie-gpt-4def2.firebaseapp.com",
+  projectId: "movie-gpt-4def2",
+  storageBucket: "movie-gpt-4def2.appspot.com",
+  messagingSenderId: "426515823881",
+  appId: "1:426515823881:web:2dfed2edc2ead95eb0e7d8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
